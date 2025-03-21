c03ec0e63c1ac5e25f4fd117dcdc38be10fadbb1
Munkhgerelpurvee <purveemunkhgerel@gmail.com>
2025-03-21 12:29:28 +0800

    Lesson40impliment pagination by usin mongoose skip and limit function

--- controller/categories.controller.js
+++ controller/categories.controller.js
@@ -9,18 +9,87 @@ const asyncHandler = require("../middleware/asyncHandler.middleware")
 
 
 exports.getCategories = asyncHandler(async (req, res, next) => {
+
+    //     
+    const page = parseInt(req.query.page) || 1;
+    // delete req.query.page;
+    // 
+    const limit1 = parseInt(req.query.limit) || 100;
+    // delete req.query.limit;
+
+    // {{url}}/api/v1/categories?select=name slug&limit=2
+
+    /*
+    { page: '3', limit: '1' } name slug undefined console дээрээс харахад page and limit нь string ирж байна. Тиймээс тоо руу хөрвүүлэх хэрэгтэй.
+
+    */
+    // 
+
     const select = req.query.select;
-    delete req.query.select;
-
+    // delete req.query.select;
+    // 
     const sort1 = req.query.sort;
-    delete req.query.sort;
+    // delete req.query.sort;
 
-        console.log(req.query, select, sort1);
+    // Энэ олон req.query-ээс устгаад байгааг хялбар болгоё. Уётгах гээд байгаа зүйлүүдээ нэг массивт хийгээд давталт гүйлгээд устгачихна буюу нэг код болгож бичиж болно.
+
+
+    ['page', 'limit', 'select', 'sort' ].forEach(el => delete req.query[el]
+    );
+
+    // Pagination тооцоолол
+    const total = await CategoryModel.countDocuments();
+    const pageCount = Math.ceil(total / limit1);
+    const start = (page -1) * limit1 + 1;
+    let end = (start + limit1) -1;
+    if(end > total) end = total;
+
+    // энэ pagination гэж обьект үүсгэе. Энэ обьект нь дараагийн болон өмнөх page-ийн талаарх мэдээллийг агуулдаг гэж үзье.Server талдаа ачаалал үүсгэхгүй гэж үзвэл энэ бүх хуудаслалтын тооцоог clint талдаа бүрэн хийж өгөх боломжтой байгаа.
+
+    const pagination = {total, pageCount, start, end, limit1};
+    if(page < pageCount) pagination.nextPage = page + 1;
+    if(page > 1) pagination.prevPage = page - 1;
+
+/*
+Одоо Pagination тооцоолол бүх зүйлүүдээ буцаагаад clint талдаа илгээх ёстой буюу ө.х   
+
+    pagination.total = total;
+    pagination.start = start;
+    pagination.end = end;
+    pagination.pageCount = pageCount; гэх мэт шинээр atribute -үүсгэж өгөөд
+
+res.status(200).json({
+            success:true,
+            data:allCategories,
+            pagination,
+            total,
+            pageCount,
+            start,
+            end
+           
+        }) энд response талдаа оруулж өгөх ёстой байгаа. Гэх мэт цувуулаад хийж өгөх боломжтой боловч
+         pagination
+
+ */
+
+
+
+
+
+
+
+
+       console.log(req.query, select, sort1, limit1);
         
-        const allCategories = await CategoryModel.find(req.query, select).sort(sort1)
+        const allCategories = await CategoryModel.find(req.query, select)
+        .sort(sort1)
+        .limit(limit1)
+        .skip(start - 1)
+
         res.status(200).json({
             success:true,
             data:allCategories,
+            pagination,
            
         })
  
@@ -93,71 +162,7 @@ exports.deleteCategory =asyncHandler(async(req, res, next) => {
 
 /*
 
-Lesson32 Өнөөдрийн хичээлээр controller дотор олон энд тэндгүй яваад байгаа try{} catch(err) {} ---эмхлээд asyncHandler гэдэг өөрсдийн бичсэн middleware дотор оруулж өгнө. Ингэснээр controller-ийн код маань илүү эмх цэгцтэй жижигхэн болж харагдах юм.
-
-exports.deleteCategory = async(req, res, next) => {
-
-    try{
-        const delCategory = await CategoryModel.findByIdAndDelete(req.params.id);
-
-        if(!delCategory) {
-            throw new MyError(req.params.id + "id-тай категори АЛГА байгаад устгаж чадаагүй ШҮҮ!!!.", 400)
-             
-         };
-    
-        res.status(200).send({
-            success:true,
-            data:req.params.id + "id-тай категорийг амжилттай устгалаа."
-        })
-
-    } catch(err) {
-      next(err);
-
-    }
-}
-
-try{} catch(err) {} --- гэдэг нь бүх газар адилхан байгаад байна. Үүнийг бид автоматжуулж болно гэсэн үг. Цаад санаа нь бол энэ функц нь цаанаа 
-
-exports.deleteCategory = async(req, res, next) => {
-        const delCategory = await CategoryModel.findByIdAndDelete(req.params.id);
-
-        if(!delCategory) {
-            throw new MyError(req.params.id + "id-тай категори АЛГА байгаад устгаж чадаагүй ШҮҮ!!!.", 400)
-             
-         };
-    
-        res.status(200).send({
-            success:true,
-            data:req.params.id + "id-тай категорийг амжилттай устгалаа."
-        })
-} үүнийг хийдэг deleteCategory нь аргументаараа       const delCategory = await CategoryModel.findByIdAndDelete(req.params.id);
-
-        if(!delCategory) {
-            throw new MyError(req.params.id + "id-тай категори АЛГА байгаад устгаж чадаагүй ШҮҮ!!!.", 400)
-             
-         };
-    
-        res.status(200).send({
-            success:true,
-            data:req.params.id + "id-тай категорийг амжилттай устгалаа."
-        }) үүнийг хийдэг энэ функцийг аргументаараа хүлээж аваад энэ функцийг try{} catch(err) {} дотор ажиллуулаад хэрвээ алдаа гараад ирэх юм бол catch-ээр нь барьж аваад бидний өмнөөс аргументаар орж ирж буй доторх next-функцийг дуудаад өгдөг тийм middleware байхад болчихно.Бид тэр middleware-ээ asyncHandler --- гэдэг нэртэй гээд үзчихье. asyncHandler чинь middleware юм чинь nst delCategory - гэдэг дээр энэ middleware-ийг буюу syncHandler функцийг  шууд холбочихож болно.  
-
-
-        Тэгвэл энэ аyncHandler функцийг  тэгвэл яаж бичих вэ?
-      аyncHandler гэдэг middleware үүсгээд : 
-      
-Доторх код нь:
-      const asyncHandler = (fn)=> (req,res,next) => 
-    Promise.resolve(fn(req,res,next)).catch(next);
-
-     module.exports = asyncHandler;
-
-     const asyncHandler функц нь аргументаараа ямар нэгэн функц хүлээж авна. Тэр аргументаар орж ирж буй (fn) функц нь цаанаа бас аргументаараа "3" аргумент хүлээж авч байгаа. Манай delCategory getCategory ---зэрэг бүх controller---ийн функцүүд маань бүгдээрээ promise буцааж байгаа буюу ӨХ async функцүүд тэр promise---ийг нь ажиллуулна гэсэн үг юм. promise-ийг нь Promise.resolve() гэдэг функцийг нь дуудаад ажиллуулчихан. Ажиллуулахдаа (fn) функц нөгөө "3" аргумент-ттай нь дуудчихна. Ингэж дуудах үед exception манай (fn) функц дотроос шидэгдэх юм бол   Promise.resolve(fn(req,res,next)).catch(next); ингэж catch ---хийж аваад next рүүгээ дамжуулна.
-
-
-
-     ийм байна. Энэ syncHandler функцийг хүмүүс ч гэсэн хийгээд үнэгүй байршуулсныг нь : expressjs asyncHandler gitHub --- гэж хэрхэн бичсэнийг олж үзээрэй.Мөн төсөл дээрээ суулгаж ажиллуулж үзэх бүрэн боломжтой.
-
+Lesson40
 
 
 
